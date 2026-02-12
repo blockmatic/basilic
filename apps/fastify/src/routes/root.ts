e1724cfe483bcf8fddb215a35dc18f5f442777fc
gaboesquivel <contact@gaboesquivel.com>
2026-02-12 02:06:11 +0000

    chore(fastify): fix runtime issues

--- apps/fastify/src/routes/root.ts
+++ apps/fastify/src/routes/root.ts
@@ -18,7 +18,7 @@ const root: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Basilic API</title>
+  <title>Basilic Fastify API</title>
   <style>
     * { margin: 0; padding: 0; box-sizing: border-box; }
     body {
